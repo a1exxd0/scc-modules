9a078cbb0b5aacbf27290222b036b42858eacbd3
Alex Do <alexxdo2004@gmail.com>
2024-06-25 20:35:23 +0100

    test: added empty parse test

--- test/argument_parsing/test_parse_arguments.cpp
+++ test/argument_parsing/test_parse_arguments.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include "command_read.h"
 
-TEST(Full_Parse_Arguments, Avail_True) {
+#pragma gcc ignore()
+TEST(Full_Parse_Arguments, Avail_Positive) {
     smod::input_arguments expected;
     expected.avail = true;
 
@@ -9,4 +10,15 @@ TEST(Full_Parse_Arguments, Avail_True) {
     auto test_case = smod::get_arguments(1, args);
 
     ASSERT_EQ(expected, test_case);
+}
+
+TEST(Full_Parse_Arguments, Empty_Parse) {
+
+    char* args[] = {nullptr};
+    EXPECT_ANY_THROW(smod::get_arguments(0, args));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
