4bf137e1b3992e5b06fb0d3daacdb1162e44e575
Alex Do <alexxdo2004@gmail.com>
2024-06-25 21:54:30 +0100

    fix: testcases didnt simulate command input correctly

--- test/argument_parsing/test_parse_arguments.cpp
+++ test/argument_parsing/test_parse_arguments.cpp
@@ -2,59 +2,66 @@
 #include "command_read.h"
 
 TEST(Full_Parse_Arguments, Empty_Parse) {
-    char* args[] = {nullptr};
-    EXPECT_THROW(smod::get_arguments(0, args), std::invalid_argument);
+    char arg[] = "smodule";
+    char* args[] = {arg};
+    EXPECT_THROW(smod::get_arguments(1, args), std::invalid_argument);
 }
 
 TEST(Full_Parse_Arguments, Keyword_As_Parameter) {
+    char arg[] = "smodule";
     char arg0[] = "avail";
     char arg1[] = "load";
-    char* args[] = {arg0, arg1};
-    EXPECT_THROW(smod::get_arguments(2, args), std::invalid_argument);
+    char* args[] = {arg, arg0, arg1};
+    EXPECT_THROW(smod::get_arguments(3, args), std::invalid_argument);
 }
 
 TEST(Full_Parse_Arguments, Avail_Positive) {
     smod::input_arguments expected;
     expected.avail = true;
 
+    char arg[] = "smodule";
     char arg0[] = "avail";
-    char* args[] = {arg0};
-    auto test_case = smod::get_arguments(1, args);
+    char* args[] = {arg, arg0};
+    auto test_case = smod::get_arguments(2, args);
 
     ASSERT_EQ(expected, test_case);
 }
 
 TEST(Full_Parse_Arguments, Avail_Negative) {
+    char arg[] = "smodule";
     char arg0[] = "avail";
     char arg1[] = "cpp15";
-    char* args[] = {arg0, arg1};
-    EXPECT_THROW(smod::get_arguments(2, args), std::invalid_argument);
+    char* args[] = {arg, arg0, arg1};
+    EXPECT_THROW(smod::get_arguments(3, args), std::invalid_argument);
 }
 
 TEST(Full_Parse_Arguments, Load_Positive) {
     smod::input_arguments expected;
     expected.load = {true, "cpp15"};
 
+    char arg[] = "smodule";
     char arg0[] = "load";
     char arg1[] = "cpp15";
-    char* args[] = {arg0, arg1};
-    auto test_case = smod::get_arguments(2, args);
+    char* args[] = {arg, arg0, arg1};
+    auto test_case = smod::get_arguments(3, args);
 
     ASSERT_EQ(test_case, expected);
 }
 
 TEST(Full_Parse_Arguments, Load_Too_Many_Args) {
+    char arg[] = "smodule";
     char arg0[] = "load";
     char arg1[] = "cpp15";
     char arg2[] = "openmpi/cpp15";
-    char* args[] = {arg0, arg1, arg2};
-    EXPECT_THROW(smod::get_arguments(3, args), std::invalid_argument);
+    char* args[] = {arg, arg0, arg1, arg2};
+    EXPECT_THROW(smod::get_arguments(4, args), std::invalid_argument);
 }
 
 TEST(Full_Parse_Arguments, Load_Too_Few_Args) {
+    char arg[] = "smodule";
     char arg0[] = "load";
-    char* args[] = {arg0};
-    EXPECT_THROW(smod::get_arguments(1, args), std::invalid_argument);
+    char* args[] = {arg, arg0};
+    EXPECT_THROW(smod::get_arguments(2, args), std::invalid_argument);
 }
 
 
